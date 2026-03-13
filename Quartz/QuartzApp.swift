57da381867fe78b40d3ba64bf81ec03d268655c9
Olli0103 <51629451+Olli0103@users.noreply.github.com>
2026-03-13 13:59:04 +0100

    Add files via upload

---
+++ Quartz/QuartzApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuartzApp.swift
+//  Quartz
+//
+//  Created by Posselt, Oliver on 13.03.26.
+//
+
+import SwiftUI
+
+@main
+struct QuartzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuartzTests/QuartzTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuartzTests.swift
+//  QuartzTests
+//
+//  Created by Posselt, Oliver on 13.03.26.
+//
+
+import Testing
+
+struct QuartzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
